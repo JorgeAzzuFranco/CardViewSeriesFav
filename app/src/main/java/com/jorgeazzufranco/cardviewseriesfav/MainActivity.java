71da73156588a49738de7f4d949a0e561b322d89
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-04-24 17:32:42 -0600

    Inicial, fragmentos vacios

--- app/src/main/java/com/jorgeazzufranco/cardviewseriesfav/MainActivity.java
+++ app/src/main/java/com/jorgeazzufranco/cardviewseriesfav/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+                Toast.makeText(getApplicationContext(), "Tab selected " +  tab.getPosition(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
